93338e699145a344bf8bc849afc101213dced579
ERUD1T3 <josiasmoukpe@gmail.com>
2020-01-19 18:43:35 -0500

    update: changed lang to python

---
+++ main.py
@@ -0,0 +1,4 @@
+# main file for unit testing
+
+if __name__ == "__main__":
+    print('Test Run!')
---
+++ packages/test.py
@@ -0,0 +1,45 @@
+# test
+
+
+class Test:
+    '''Test class for prog unit testing'''
+
+    def __init__(self, prog_output_file):
+        '''initializes a test object for a test program based on output file'''
+        self.filename = prog_output_file
+
+        with open(self.filename, 'r') as f:
+            # reading the first line in as the total number of pairs
+            self.pairs_num = f.readline()
+
+            # setting up the data structure to containt the pairs
+            self.data = {i: [] for i in range(1, self.pairs_num + 1)}
+
+            for line in f:
+                # looping through the file to store the data
+                i, o = line.split()
+                self.data[i].append(o)
+
+    def isOnto(self):
+        '''return true if the prog is onto'''
+        return True
+
+    def isOne2One(self):
+        '''return true if the prog is one2one'''
+        return True
+
+    def isReflexive(self):
+        '''return true if the prog is reflexive'''
+        return True
+
+    def isSymmetric(self):
+        '''return true if the prog is symmetric'''
+        return True
+
+    def isTransitive(self):
+        '''return true if the prog is Transitive'''
+        return True
+
+    def isFunction(self):
+        '''return true if the prog is a function'''
+        return True
