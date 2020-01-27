1c5354399f95819e01bd7b25ce8f7418940a4b47
ERUD1T3 <josiasmoukpe@gmail.com>
2020-01-27 17:40:58 -0500

    update:  made single tester file

--- exe/tester.py
+++ exe/tester.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # test
 
 from bisect import insort
@@ -166,3 +168,26 @@ class Test:
             idx += 1
 
         print('Transitive' if res else 'Not transitive')
+
+
+def main():
+    # prog_test = Test(sys.argv[1])
+    prog_test = Test()
+
+    print('Running tests...')
+
+    prog_test.isOnto()
+    prog_test.isOne2one()
+    prog_test.isReflexive()
+    prog_test.isSymmetric()
+    prog_test.isTransitive()
+    prog_test.isFunction()
+    prog_test.isFunction('onto')
+    prog_test.isFunction('one2one')
+
+    print('Test complete!')
+
+
+if __name__ == "__main__":
+    main()
+    # print(sys.argv)
